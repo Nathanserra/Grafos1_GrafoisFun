f11318820f27ce8ea6c59ea8014ce2469c12cd0f
vitordiniz25 <vitordiniz25@gmail.com>
2021-08-04 00:01:15 -0300

    Inputs

--- minha-grade/src/App.js
+++ minha-grade/src/App.js
@@ -2,13 +2,26 @@ import './Styles.css';
 import React from 'react';
 
 function App() {
+  const graphInfoForm = () => (
+    <div className="inputForm">
+      <input
+        type="text"
+        placeholder="Número de Vértices"
+      />
+      <input
+        type="text"
+        placeholder="Número de Arestas"
+      />
+    </div>
+  )
 
   return (
     <div className="header">
       <h1>Graphs is fun!</h1>
-      <div>Este projeto serve para mostrar um pouco das muitas funcionalidades de grafos
+      <div className="subtitle">Este projeto serve para mostrar um pouco das muitas funcionalidades de grafos
         e também para mostrar o quão divertido "brincar" com grafos pode ser.
       </div>
+      <div>{graphInfoForm()}</div>
     </div>
   );
 }
