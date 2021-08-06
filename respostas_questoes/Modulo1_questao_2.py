b197cca629491f6062872cb90f76ffa577459387
Nathanserra <nathanserra99@gmail.com>
2021-08-05 23:50:57 -0300

    arrumando função e comentando codigo

--- respostas_questoes/Modulo1_questao_2.py
+++ respostas_questoes/Modulo1_questao_2.py
@@ -65,10 +65,8 @@ class Grafo:
 
     
     def DFS_retorna_ciclo(self):
-        G_aux = Grafo(self.V,{(1,2)})
-        G_ciclo = Grafo(self.V,{(1,2)})
-        G_aux.apaga_aresta(1,2)
-        G_ciclo.apaga_aresta(1,2)
+        G_aux = Grafo(self.V,{})
+        G_ciclo = Grafo(self.V,{})
 
         nao_visitados = self.V
         for v in self.V:
@@ -98,9 +96,8 @@ class Grafo:
                 self.acha_ciclo(G_ciclo, G_aux, w, list(G_aux.vizinhos(v))[0])
 
 if __name__ == '__main__':
-    G = Grafo([1,2,3,4,5,6,7,8], {(1,2),(1,3),(3,2),(3,8),(3,7),(2,5),(2,4),(4,6)})
+    G = Grafo([1,2,3,4,5,6,7,8,9,10,11], {(1,2),(2,3),(3,4),(3,5),(4,8),(5,6),(6,7),(7,8),(7,9),(9,11),(11,10),(10,8)})
     G1 = G.DFS_retorna_ciclo()
-
     for i in range(G1.n):
         print(f'{i+1}: ', end=' ')
         print(list(G1.vizinhos(i+1)))
