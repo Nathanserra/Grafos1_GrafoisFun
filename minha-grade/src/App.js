ff442232f06d9df344cb0543b94993c636e83f3c
vitordiniz25 <vitordiniz25@gmail.com>
2021-08-06 09:59:13 -0300

    Adicionando estrutura de para criacao dos grafos

--- minha-grade/src/App.js
+++ minha-grade/src/App.js
@@ -3,6 +3,23 @@ import React from 'react';
 import { Stage, Layer, Arrow, Circle, Text } from "react-konva";
 
 function App() {
+  // function createGraph() {
+  //   if (vertex <= 0 || vertex > 1000 || isNaN(vertex)) {
+  //     alert("Número de vértices inválido!!!")
+  //     return
+  //   }
+  //   if (edges <= 0 || edges > 1000 || isNaN(edges)) {
+  //     alert("Número de arestas inválido!!!")
+  //     return
+  //   }
+  //   if (edges > vertex * (vertex - 1) && edges > 0) {
+  //     alert("Número de arestas inválido!!!\n Lembrete: Número de arestas é menor ou igual ao número de Vértices*(Vértices-1)")
+  //     return;
+  //   }
+
+  //   //Funçao de inicializar grafo aqui!!!
+  // }
+
   const graphInfoForm = () => (
     <div className="inputForm">
       <div>
@@ -11,13 +28,13 @@ function App() {
           type="text"
           placeholder="Número de Vértices"
         />
-        <button>novo</button>  
+        <button>novo</button>
       </div>
       <div>
         <h3>Adicione as arestas</h3>
-        <input className="aresta"/>
-        <input className="aresta"/>
-        <button>adicionar</button>  
+        <input className="aresta" />
+        <input className="aresta" />
+        <button>adicionar</button>
       </div>
     </div>
   )
@@ -26,9 +43,9 @@ function App() {
     <div className="inputSearch">
       <div>
         <h3>Ache o menor caminho!!!</h3>
-        <input className="aresta"/>
-        <input className="aresta"/>
-        <button>procurar</button>  
+        <input className="aresta" />
+        <input className="aresta" />
+        <button>procurar</button>
       </div>
     </div>
   )
